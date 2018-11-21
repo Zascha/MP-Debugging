30be5ffcd5b24e5963ab9a193c319ac2fdfe6023
MINSK\Aliaksandra_Zakharav <Aliaksandra_Zakharava@epam.com>
2018-11-21 11:10:46 +0300

    Version 11/21

---
+++ MP.Debugging/MP.Debugging.CodeGen/CodeGererator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+
+namespace MP.Debugging.CodeGen
+{
+    internal static class CodeGererator
+    {
+        private static string KeyDelimiter = "-";
+
+        public static string GetCrackKey()
+        {
+            var networkBytes = GetNetworkInterfaceBytes();
+            var dateBytes = GetCurrentDateBytes();
+
+            var keyItems = networkBytes.Zip(dateBytes, (i, b) => i ^ b).Select(b => b * 10);
+
+            return GetFormatedKey(keyItems);
+        }
+
+        #region Private methods
+
+        private static int[] GetNetworkInterfaceBytes()
+        {
+            var networkInterface = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+            if (networkInterface == null)
+            {
+                throw new ApplicationException("Network interface required");
+            }
+
+            var networkInterfaceBytes = networkInterface.GetPhysicalAddress().GetAddressBytes();
+
+            return ToIntArray(networkInterfaceBytes);
+        }
+
+        private static int[] GetCurrentDateBytes()
+        {
+            return ToIntArray(BitConverter.GetBytes(DateTime.Now.Date.ToBinary()));
+        }
+
+        private static int[] ToIntArray(byte[] byteArray)
+        {
+            return byteArray.Select(byteItem => (int)byteItem).ToArray();
+        }
+
+        private static string GetFormatedKey(IEnumerable<int> keyItems)
+        {
+            return string.Join(KeyDelimiter, keyItems);
+        }
+
+        #endregion
+    }
+}
---
+++ MP.Debugging/MP.Debugging.CodeGen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MP.Debugging.CodeGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crackCode = CodeGererator.GetCrackKey();
+            Console.WriteLine($"Your crack key is: {crackCode}");
+
+            Console.ReadKey();
+        }
+    }
+}
